d8adab00c5568e18db9816ef75aea2c8bf63414e
James Rine <jlrine2@alaska.edu>
2021-06-08 22:18:48 -0800

    add versioning code

--- sfreeze/__init__.py
+++ sfreeze/__init__.py
@@ -1,4 +1,17 @@
+from importlib.metadata import PackageNotFoundError, version
+
 from freeze import s3_freeze, s3_thaw
+
+
+try:
+    __version__ = version(__name__)
+except PackageNotFoundError:
+    print('package is not installed!\n'
+          'Install in editable/develop mode via (from the top of this repo):\n'
+          '   python -m pip install -e .\n'
+          'Or, to just get the version number use:\n'
+          '   python setup.py --version')
+
 
 __all__ = [
     's3_freeze',
